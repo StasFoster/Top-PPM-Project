d2b8eec181ab08adcc4eccc18234fbe9c0a89dc6
StasFoster <stanislav06052005@gmail.com>
2026-02-25 15:49:57 +0400

    добавили джанго пользователя

--- Modules/Socnet/views.py
+++ Modules/Socnet/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import MyUser
 from .forms import MyUserForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 # from django.contrib.auth.models import User
 
 def main(request):
@@ -18,29 +18,25 @@ def register(request):
 
     return render(request, "Socnet/register.html")
 
-def get_user_data(request):
-    nickname = request.POST.get("nickname")
-    username = request.POST.get("username")
-    password = request.POST.get("password")
-    pass_confirm = request.POST.get("password_confirm")
+def get_user_data(request):   
+    form = MyUserForm(data=request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request,user)
 
-    # sport = request.POST.get("sport")
-    # video_game = request.POST.get("video_game")
-    # book = request.POST.get("book")
-    # films = request.POST.get("films")
-    # automobile = request.POST.get("automobile")
-
-    list_interests = ["sport","video_game","book","films","automobile"]
-    interests = {}
-    for i in range(1, len(list_interests) + 1):
-        interest = request.POST.get(list_interests[i - 1])
-        if interest == "on":
-            interests[i] = 1
-        else:
-            interests[i] = 0
+        list_interests = ["sport","video_game","book","films","automobile"]
+        interests = {}
+        for i in range(1, len(list_interests) + 1):
+            interest = request.POST.get(list_interests[i - 1])
+            if interest == "on":
+                interests[i] = 1
+            else:
+                interests[i] = 0
+        
+        user.interests = interests
+        user.save()
     
-    if password != pass_confirm:
-        return  redirect("register")
+
     
     
     user = MyUser() # user = User()
@@ -60,6 +56,7 @@ def test(request):
         form = MyUserForm(data=request.POST)
         if form.is_valid():
             user = form.save()
+            login(request,user)
 
     else:
         form = MyUserForm()
