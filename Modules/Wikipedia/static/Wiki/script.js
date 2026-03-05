f14617c0987bbc0eadaa34c3b2e1bffd4d7eec6a
StasFoster <stanislav06052005@gmail.com>
2026-03-05 12:59:32 +0400

    Add first Article

--- Modules/Wikipedia/static/Wiki/script.js
+++ Modules/Wikipedia/static/Wiki/script.js
@@ -1,10 +1,13 @@
-btn1 = document.getElementById("s1")
-title = document.getElementById("title")
-disc = document.getElementById("disc")
-
-btn1.getEventListener("click", function(){
+btn1 = document.getElementById("s1");
+title = document.getElementById("title");
+disc = document.getElementById("disc");
+console.log("_____2_____");
+btn1.addEventListener("click", function(){
     fetch("http://127.0.0.1:8000/wiki/api")
-        .then((data) => data.json())
-        .then((t) => title.innerText = t["title"])
-        .then((t) => disc.innerText = t["disc"])
+        .then((response) => response.json())
+        .then((data) => {
+            title.innerText = data["title"];
+            disc.innerText = data["disc"];
+        })
+        .catch((error) => console.error('Ошибка:', error));
 })
--- Modules/Wikipedia/views.py
+++ Modules/Wikipedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from . import models
 
@@ -8,14 +8,18 @@ def add(request):
     s1 = models.Article()
     s1.title = "Stas"
     s1.disc = "asdfghjk"
+    s1.save()
+    print("asdfgh")
+    return redirect("wiki_main")
 
 def main(request):
-    return render(request, "Wiki/imdex.html")
+    return render(request, "Wiki/index.html")
 
 def api_art(request):
-    s1 = models.Article.objects.get(title="Stas")
+    s1 = models.Article.objects.all()[0]
     data = {
         "title" : s1.title,
         "disc" : s1.disc,
     }
+    print("________1________")
     return JsonResponse(data)
